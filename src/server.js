557ac79de5f6468f68b7281e99fb2506d079ed31
NonaEarth <hsoj4129@gmail.com>
2022-12-03 22:37:05 +0900

    Update

--- src/server.js
+++ src/server.js
@@ -22,10 +22,10 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 
 app.use((req, res, next) => {
-	res.header("Cross-Origin-Embedder-Policy", "require-corp");
-	res.header("Cross-Origin-Opener-Policy", "same-origin");
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	res.header("Cross-Origin-Embedder-Policy", "require-corp");
+	res.header("Cross-Origin-Opener-Policy", "same-origin");
 	next();
 });
 
