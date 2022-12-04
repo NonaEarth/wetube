3d4db7f71391f38ad2d1e6b4c9341547818e084e
NonaEarth <hsoj4129@gmail.com>
2022-12-04 18:25:05 +0900

    Update

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -138,8 +138,8 @@ export const postLogin = async function (req, res) {
 };
 
 export const logout = function (req, res) {
-	req.session.destroy();
-	// req.flash("info", "You're logged out.");
+	req.session.loggedIn = false; 
+	req.flash("info", "You're logged out.");
 	return res.redirect("/");
 };
 
