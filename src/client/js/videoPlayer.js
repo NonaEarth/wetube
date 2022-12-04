7bee2b4505b7515b08dd6af55a05c53867adbcc6
NonaEarth <hsoj4129@gmail.com>
2022-12-04 19:04:15 +0900

    Update

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -83,8 +83,8 @@ const handleVolumeChange = function (event) {
 };
 
 const handleLoadedMetadata = function () {
-    totalTime.innerText = formatTime(Math.floor(video.duration));
-    timeline.max = Math.floor(video.duration);
+	totalTime.innerText = formatTime(Math.floor(video.duration));
+	timeline.max = Math.floor(video.duration);
 };
 
 const handleTimeUpdate = function () {
@@ -144,7 +144,7 @@ const handleEnded = function () {
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMuteClick);
 volumeRange.addEventListener("input", handleVolumeChange);
-video.addEventListener("loadeddata", handleLoadedMetadata);
+video.addEventListener("loadedmetadata", handleLoadedMetadata);
 video.addEventListener("timeupdate", handleTimeUpdate);
 video.addEventListener("ended", handleEnded);
 timeline.addEventListener("input", handleTimelineChange);
@@ -159,3 +159,7 @@ document.addEventListener("keydown", (event) => {
 		handleFullScreen();
 	}
 });
+
+video.readyState
+	? handleLoadedMetadata()
+	: video.addEventListener("loadedmetadata", handleLoadedMetadata);
